48030acef37aded13fff9c8c62e6d69fe3ac0e63
2411500014-design <2411500014@mahasiswa.atmaluhur.ac.id>
2026-04-24 17:58:42 +0700

    tugas5

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      home: CounterPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }t
+}
+
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int counter = 0;
+
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      counter--;
+    });
+  }
+
+  void reset() {
+    setState(() {
+      counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter App'), centerTitle: true),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Nilai Counter:', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 10),
+            Text(
+              '$counter',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: decrement, child: Text('-')),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: reset, child: Text('Reset')),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: increment, child: Text('+')),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
